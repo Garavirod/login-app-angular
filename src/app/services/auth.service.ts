f3021d09466b9cc37a1d16c055e3f0073d15b2e3
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-08-12 15:50:14 -0500

    Create

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,14 +6,12 @@ import { UserModel } from '../models/User.model';
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:';
   private apikey = 'AIzaSyAlRNeAvqiZqEjAeMf5dE_6h9-J0gKrhAM';
   // Create new users
   // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
   // Login
   // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   logout() {  
   }
