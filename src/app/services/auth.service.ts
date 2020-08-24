16e65287263c5cf25cf082089cebab2b7f071945
Garavirod <rodrigogarciaavila26@gmail.com>
2020-08-24 18:53:43 -0500

    local storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,35 +1,61 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserModel } from '../models/User.model';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apikey = 'AIzaSyAlRNeAvqiZqEjAeMf5dE_6h9-J0gKrhAM';
+  private userToken;
   // Create new users
   // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
   // Login
   // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
-  constructor(private http: HttpClient) {}
-
-  logout() {  
+  constructor(private http: HttpClient) { 
+    this.getToken();
   }
-  
+
+  logout() {
+  }
+
   login(user: UserModel) {
     const authData = {
       ...user,
       returnSecureToken: true
     };
 
-    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apikey}`,authData);
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apikey}`, authData)
+      .pipe(
+        map(res => {
+          console.log("Inside map RSJX");
+          this.saveToken(res['idToken']);
+          return res;
+        })
+      );
   }
   signup(user: UserModel) {
     const authData = {
-      email : user.email,
+      email: user.email,
       password: user.password,
-      returnSecureToken : true
+      returnSecureToken: true
     };
-    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ this.apikey }`, authData );
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.apikey}`, authData)
+    .pipe(
+      map(res => {
+        console.log("Inside map RSJX");
+        this.saveToken(res['idToken']);
+        return res;
+      })
+    );
+  }
+
+  private saveToken(idToken: string) {
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+  }
+
+  private getToken() {
+    this.userToken = (localStorage.getItem('token')) ? localStorage.getItem('token') : '';
   }
 }
